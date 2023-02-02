d2ab72f080e54d4e4b5ac5e49adc86a89122e7f2
cliffxzx <cliffxzx@gmail.com>
2023-02-02 18:36:02 +0800

    Reorganize files

--- validation.py
+++ validation.py
@@ -1,5 +1,5 @@
 def main():
-  with open("valid.txt", "w") as out:
+  with open("inputs/valid.txt", "w") as out:
     for i in range(10, 501, 5):
       with open(f"inputs/{i}.in", "r") as fin:
         out.write(str(int(fin.readline()) * int(fin.readline())) + '\n')
